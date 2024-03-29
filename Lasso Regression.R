e0f1cf03622c5beee2cbadb19ff336b00a396cf4
matt4ta <57694978+matt4ta@users.noreply.github.com>
2024-03-29 18:00:23 -0400

    Added comparison of models using validation set

--- Lasso Regression.R
+++ Lasso Regression.R
@@ -1,11 +1,21 @@
 library(glmnet)
 
-# Prepare data for model creation
 BikeData <- readData()
+
+# Only consider functioning days
+# BikeData <- BikeData[BikeData$Functioning_Day == 1, ]
+# BikeData$Functioning_Day <- NULL
+
+# BikeData$Spring <- NULL
+# BikeData$Summer <- NULL
+# BikeData$Autumn <- NULL
+# BikeData$Winter <- NULL
+
+# Prepare data for model
 X <- model.matrix(Rented_Bikes ~ ., BikeData)
 Y <- BikeData$Rented_Bikes
 
-# Create training and testing data sets
+# Create training and validation data sets
 set.seed(1)
 train <- sample(1:nrow(X), nrow(X) * 4 / 5)
 test = (-train)
@@ -23,7 +33,42 @@ lasso.mod$beta
 lasso.pred <- predict(lasso.mod, s = bestlam, newx = X[test, ], type = "response")
 
 # calculate MSE
-lasso.MSE <- mean((lasso.pred - Y[test]))^2
+lasso.MSE <- mean((lasso.pred - Y[test])^2)
 lasso.MSE
 
+# calculate R-squared
+lasso.RSS <- sum((lasso.pred - Y[test])^2)
+TSS <- sum(Y[test]^2)
+lasso.r2 <- 1 - lasso.RSS / TSS
+
+# calculate Adjusted R-squared
+N <- dim(BikeData[test, ])[1]
+p <- dim(BikeData[test, ])[2]
+lasso.adjr2 <- 1 - (1 - lasso.r2^2) * (N - 1) / (N - p - 1)
+
+
+#lasso.pred
+
+# create linear model based on training data
+lm.fit <- lm(Rented_Bikes ~ ., data = BikeData[train, ])
+
+# apply model to validation set
+lm.pred <- predict(lm.fit, BikeData[test, ])
+
+#calculate linear model MSE
+lm.MSE <- mean((lm.pred - Y[test])^2)
+
+# calculate linear model r-squared
+lm.RSS <- sum((lm.pred - Y[test])^2)
+lm.r2 <- 1 - lm.RSS / TSS
+
+# calculate linear model Adjusted R-Squared
+lm.adjr2 <- 1 - (1 - lm.r2^2) * (N - 1) / (N - p - 1)
+
+
+# output error values
+lasso.MSE
+lasso.adjr2
+lm.MSE
+lm.adjr2
 
